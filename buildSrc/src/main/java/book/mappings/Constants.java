23fdcb5403f7145f85053c99db8291d2f83dc13a
CrypticVerse <crypticversemc@gmail.com>
2024-09-18 17:08:46 -0400

    24w38a

--- buildSrc/src/main/java/book/mappings/Constants.java
+++ buildSrc/src/main/java/book/mappings/Constants.java
@@ -1,7 +1,7 @@
 package book.mappings;
 
 public class Constants {
-    public static final String MINECRAFT_VERSION = "24w37a";
+    public static final String MINECRAFT_VERSION = "24w38a";
 
     public static final String MAPPINGS_NAME = "book-mappings";
 
